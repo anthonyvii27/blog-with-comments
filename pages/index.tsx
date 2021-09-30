f3a4e531cd61dd087bec21ff780b5951e9755d5c
Anthony <anthony.vii27@gmail.com>
2021-09-30 14:24:51 -0300

    refactor: removed index.ts content from posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
-import type { NextPage } from 'next';
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import Link from 'next/link';
 
-const Home: NextPage = () => {
+import { IPostList } from '../interfaces/IPosts';
+import { getAllPosts } from '../lib/getPosts';
+
+const Home: NextPage = ({ allPosts }: InferGetStaticPropsType<typeof getStaticProps>) => {
     return (
-        <h1>Olá</h1>
+        <main>
+            {allPosts?.length ? 
+                allPosts?.map((post: IPostList) => (
+                    <article key={post?.slug}>
+                        <Link href={`/posts/${post?.slug}`}>
+                            <a>{post?.title}</a>
+                        </Link>
+
+                        <p>{post?.author}</p>
+
+                        <time>{post?.date}</time>
+                    </article>
+                ))
+                
+                : <p>Nenhum post publicado até o momento</p>
+            }
+        </main>
     );
 }
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+    const allPosts = getAllPosts(['slug', 'title', 'author', 'date']);
+
+    return {
+        props: { allPosts },
+    }
+}
