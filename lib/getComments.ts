391b8186624cc600cdf3159b9b3d722e42205c3b
Anthony <anthony.vii27@gmail.com>
2021-09-30 21:17:18 -0300

    feat: get comments and show on post page

--- lib/getComments.ts
+++ lib/getComments.ts
@@ -1,7 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { connectToDatabase } from '../config/mongodb';
 
 async function GetComments(req: NextApiRequest, res: NextApiResponse) {
+    const { db } = await connectToDatabase();
+    
+    const slug = req.query.slug; 
 
+    if(!slug) {
+        return res.status(400).json({ message: 'Invalid parameters' });
+    }
+
+    try {
+        const comments = await db.collection('comments').find({ slug }).toArray();
+        return res.status(200).json({ comments });
+    
+    } catch(_) {
+        return res.status(400).json({ message: 'Unexpected error occurred' });
+    }
 }
 
 export default GetComments;
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps, GetStaticPaths, InferGetStaticPropsType } from 'next';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import ErrorPage from 'next/error';
-import { useState } from 'react';
 import Head from 'next/head';
 
 import { getAllPosts, getPostBySlug } from '../../lib/getPosts';
@@ -9,11 +9,30 @@ import markdownToHtml from '../../lib/parseMarkdownToHTML';
 
 import { IPost } from '../../interfaces/IPosts';
 
+type Comment = {
+    id: string,
+    username: string,
+    text: string,
+    created_at: Date,
+    slug: string
+}
+
 const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
     const router = useRouter();
 
+    const [commentsList, setCommentsList] = useState<any>([]);
     const [username, setUsername] = useState('');
     const [text, setText] = useState('');
+
+    useEffect(() => {
+        (async function () {
+            const res = await fetch(`/api/comment?slug=${post?.slug}`, {
+                method: 'GET'
+            });
+
+            res.json().then((comments ) => setCommentsList(comments));
+        })();
+    }, [post?.slug]);
 
     if (!router.isFallback && !post?.slug) {
         return <ErrorPage statusCode={404} />
@@ -29,7 +48,10 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-            })
+            });
+
+            router.reload();
+            
         } catch(_) {
             throw new Error('Unexpected error occurred')
         }
@@ -63,14 +85,27 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
                             className="prose mt-10"
                             dangerouslySetInnerHTML={{ __html: post?.content }}
                         />
+
+                        <h1>Comentários</h1>
+
+                        <section>
+                            {commentsList?.comments?.map((comment: Comment) => (
+                                <div key={comment.id}>
+                                    <span>{comment.username} - {comment.created_at}</span>
+                                    <p>{comment.text}</p>
+                                </div>
+                            ))}
+                        </section>
+
+                        <form onSubmit={createComment}>
+                            <input type="text" placeholder="Seu nome" onChange={e => setUsername(e.target.value)} />
+                            <input type="text" placeholder="Comentário" onChange={e => setText(e.target.value)} />
+
+                            <button type="submit">Enviar</button>
+                        </form>
                     </article>
 
-                    <form onSubmit={createComment}>
-                        <input type="text" placeholder="Seu nome" onChange={e => setUsername(e.target.value)} />
-                        <input type="text" placeholder="Comentário" onChange={e => setText(e.target.value)} />
-
-                        <button type="submit">Enviar</button>
-                    </form>
+                                       
                 </div>
             }
         </main>
